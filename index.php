5054e54326f04bad7f12d11c684c58077a8531f7
masukmia <dev.masuk@gmail.com>
2022-07-24 21:25:49 +0600

    init

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body <?php body_class(); ?>>
+	<div id="page" class="site">
+		<header>
+			<section class="search">Search</section>
+			<section class="top-bar">
+				<div class="brand">Logo</div>
+				<div class="second-column">
+					<div class="acount">Account</div>
+					<nav class="main-menu">Menu</nav>
+				</div>
+			</section>
+		</header>
+		<div class="content-area">
+			<main>
+				<section class="slider">Slider</section>
+				<section class="popular-products">Popular Products</section>
+				<section class="new-arrivals">New Arrivals</section>
+				<section class="deal-of-the-week">Deal of the Week</section>
+				<section class="lab-blog">News</section>
+			</main>
+		</div>
+		<footer>
+			<section class="footer-widgets">Footer Widgets</section>
+			<section class="copyright">Copyright</section>
+		</footer>
+	</div>
+</body>
+</html>
