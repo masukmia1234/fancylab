63be29fe9e23dece132261a44173d5ef78ae3d9a
masukmia <dev.masuk@gmail.com>
2022-08-17 19:42:43 +0600

    name changes

--- inc/wc-modification.php
+++ inc/wc-modification.php
@@ -1,55 +1,67 @@
-<?php
-
+<?php 
 /**
- * The main template file
+ * Template Overrides for WooCommerce pages
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @link https://docs.woocommerce.com/document/woocommerce-theme-developer-handbook/#section-3
  *
  * @package Fancy Lab
  */
+ 
+function fancy_lab_wc_modify(){
+	/** 
+	* Modify WooCommerce opening and closing HTML tags
+	* We need Bootstrap-like opening/closing HTML tags
+	*/
+	add_action( 'woocommerce_before_main_content', 'fancy_lab_open_container_row', 5 );
+	function fancy_lab_open_container_row(){
+		echo '<div class="container shop-content"><div class="row">';
+	}
+
+	add_action( 'woocommerce_after_main_content', 'fancy_lab_close_container_row', 5 );
+	function fancy_lab_close_container_row(){
+		echo '</div></div>';
+	}
+
+	/** 
+	* Remove the main WC sidebar from its original position
+	* We'll be including it somewhere else later on
+	*/
+	remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar' );
+
+	if( is_shop() ){
+
+		add_action( 'woocommerce_before_main_content', 'fancy_lab_add_sidebar_tags', 6 );
+		function fancy_lab_add_sidebar_tags(){
+			echo '<div class="sidebar-shop col-lg-3 col-md-4 order-2 order-md-1">';
+		}
+
+		// Put the main WC sidebar back, but using other action hook and on a different position
+		add_action( 'woocommerce_before_main_content', 'woocommerce_get_sidebar', 7 );
+
+		add_action( 'woocommerce_before_main_content', 'fancy_lab_close_sidebar_tags', 8 );
+		function fancy_lab_close_sidebar_tags(){
+			echo '</div>';
+		}
+
+		add_action( 'woocommerce_after_shop_loop_item_title', 'the_excerpt', 1 );		
+	}
 
 
-add_action( 'after_setup_theme', 'fancy_lab_config', 0 );
-add_action( 'woocommerce_before_main_content', 'fancy_lab_open_container_row', 5 );
-function fancy_lab_open_container_row(){
-	echo '<div class="container shop-content"><div class="row">';
+	// Modify HTML tags on a shop page. We also want Bootstrap-like markup here (.primary div)
+	add_action( 'woocommerce_before_main_content', 'fancy_lab_add_shop_tags', 9 );
+	function fancy_lab_add_shop_tags(){
+		if( is_shop()){
+			echo '<div class="col-lg-9 col-md-8 order-1 order-md-2">';
+		} else{
+			echo '<div class="col">';
+		}
+		
+	}
+
+	add_action( 'woocommerce_after_main_content', 'fancy_lab_close_shop_tags', 4 );
+	function fancy_lab_close_shop_tags(){
+		echo '</div>';
+	}
+		
 }
-
-add_action( 'woocommerce_after_main_content', 'fancy_lab_close_container_row', 5 );
-function fancy_lab_close_container_row(){
-	echo '</div></div>';
-}
-
-add_action( 'woocommerce_before_main_content', 'fancy_lab_add_sidebar_tags', 6 );
-function fancy_lab_add_sidebar_tags(){
-	echo '<div class="sidebar-shop col-lg-3 col-md-4 order-2 order-md-1">';
-}
-
-remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar' );
-add_action( 'woocommerce_before_main_content', 'woocommerce_get_sidebar', 7 );
-
-add_action( 'woocommerce_before_main_content', 'fancy_lab_close_sidebar_tags', 8 );
-function fancy_lab_close_sidebar_tags(){
-	echo '</div>';
-}
-
-add_action( 'woocommerce_before_main_content', 'fancy_lab_add_shop_tags', 9 );
-function fancy_lab_add_shop_tags(){
-	echo '<div class="col-lg-9 col-md-8 order-1 order-md-2">';
-}
-
-add_action( 'woocommerce_after_main_content', 'fancy_lab_close_shop_tags', 4 );
-function fancy_lab_close_shop_tags(){
-	echo '</div>';
-}
-add_filter( 'woocommerce_show_page_title', 'fancy_lab_remore_shop_title' );
-function fancy_lab_remore_shop_title(){
-	$val=false;
-	return $val;
-}
-add_action( 'woocommerce_after_shop_loop_item_title', 'the_excerpt',1 );
+add_action( 'wp', 'fancy_lab_wc_modify' );
